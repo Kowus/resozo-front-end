e46c0c082b0155423a9f05e5c68f5dbbb9455785
Kowus <barnabas@elite-education.org>
2018-05-23 12:34:08 +0000

    inits

--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import './styles/quasar.styl'
+import 'quasar-framework/dist/quasar.ie.polyfills'
+import iconSet from 'quasar-framework/icons/fontawesome'
+import 'quasar-extras/animate'
+import 'quasar-extras/roboto-font'
+import 'quasar-extras/fontawesome'
+import Quasar, * as All from 'quasar'
+
+Vue.use(Quasar, {
+  iconSet: iconSet,
+  components: All,
+  directives: All,
+  plugins: All
+})
+
 Vue.config.productionTip = false
 
 new Vue({
